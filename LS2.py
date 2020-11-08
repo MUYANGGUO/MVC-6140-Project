2c901d8efdcd9a44d98ded4893ade6ab71f96c20
MUYANG GUO <muyangguo@gmail.com>
2020-11-08 00:56:22 -0500

    update

--- LS2.py
+++ LS2.py
@@ -1,3 +1,11 @@
-def LS2(graph, time, seed):
-    print('LS2 local search ### method\n')
+import numpy as np
+import time
+def LS2(graph, cut_time, seed):
+    print('LS2 local search Simulated Annealing method\n')
+    # initialization
+    np.random.seed(seed)
+    vertex_set = set(graph.nodes)
+    start_time = time.time()
+    while time.time() - start_time < cut_time:
+        pass
     pass
