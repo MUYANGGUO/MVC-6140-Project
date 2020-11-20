342af5695ee197580dd2738496882c03fb9abd60
MUYANGGUO <muyangguo@gmail.com>
2020-11-19 20:57:30 -0500

    update cutime sequence

--- experiments.sh
+++ experiments.sh
@@ -3,7 +3,7 @@ echo "performing experiments in a grid search fashion ... \n"
 datafiles=`ls ./DATA/*.graph`
 for datafile in $datafiles
 do
-    for CUTTIME in 10.0 50.0 100.0 500.0
+    for CUTTIME in 10.0 50.0 250.0 500.0
     do
         python3 MVC.py $datafile LS2 $CUTTIME 1
     done
