ab7c4a5be04334ec269aeaf83efdce5040da39f3
MUYANGGUO <muyangguo@gmail.com>
2020-11-27 20:35:01 -0500

    update

--- experiments.sh
+++ experiments.sh
@@ -3,7 +3,7 @@ echo "performing experiments in a grid search fashion ... \n"
 datafiles=`ls ./DATA/*.graph`
 for datafile in $datafiles
 do
-    for CUTTIME in 10.0 50.0 250.0 500.0
+    for CUTTIME in 10.0 50.0 100.0 500.0 1000.0
     do
         python MVC.py $datafile Approx $CUTTIME 1
     done
