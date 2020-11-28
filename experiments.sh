2ec1a6a9320d05371e5ec1085fadda151be92f66
MUYANGGUO <muyangguo@gmail.com>
2020-11-28 09:57:06 -0500

    updates with more data

--- experiments.sh
+++ experiments.sh
@@ -5,6 +5,6 @@ for datafile in $datafiles
 do
     for CUTTIME in 10.0 50.0 100.0 500.0 1000.0
     do
-        python MVC.py $datafile Approx $CUTTIME 1
+        python3 MVC.py $datafile LS2 $CUTTIME 1
     done
 done
