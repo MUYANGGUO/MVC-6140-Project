83a092d8cbbd4b2531a2d466d0182dd2057a368f
yijiao <yjiao47@gatech.edu>
2020-12-01 17:22:29 +0800

    fix bug

--- Approx.py
+++ Approx.py
@@ -2,60 +2,30 @@ import time
 import sys
 sys.setrecursionlimit(100000)
 
-def Approx(graph, cutoff_time, seed):
-    print('Approximate method\n')
 
-    success, runtime, vertex_cov, vc_size = dfsMVC(graph, cutoff_time)
-    if not success:
-        print("No enough cutoff time to produce a result.\n")
+def Approx(graph, cutoff_time, seed):
+    start = time.time()
+    print('Approximation method\n')
+    visited = set()
+    num_vertex = len(graph.nodes)
+    non_leaf = set()
+
+    for i in range(1, num_vertex + 1):
+        if i not in visited:
+            dfs(i, visited, num_vertex, graph, non_leaf)
+
+    runtime = time.time() - start
+    trace = [str(runtime) + ',' + str(len(non_leaf))]
+    if cutoff_time < runtime:
         return set(), []
     else:
-        trace = []
-        vertex_cover = set()
-        for i in range(1, vc_size):
-            vertex_cover.add(vertex_cov[i])
-        trace.append(str(runtime) + ',' + str(vc_size))
-        return vertex_cover, trace
+        return non_leaf, trace
 
 
-def dfsMVC(graph, cutoff_time):
-    start = time.time()
-    vertex_cov = []
-    visited = set()
-    visited.add(0)
-    num_vertex = len(graph.nodes)
-    vc_size = 0
-    success = True
-    for i in range(1, num_vertex):
-        vc_size = dfs(i, visited, graph, vertex_cov, num_vertex, vc_size, start, cutoff_time)
-        if time.time() - start > cutoff_time:
-            success = False
-            break
-    if success:
-        end = time.time()
-        runtime = end-start
-        return success, runtime, vertex_cov, vc_size
-    else:
-        return success, 0, vertex_cov, vc_size
-
-
-def dfs(i, visited: set, graph, vertex_cov: list, num_vertex: int, vc_size: int, start, cutoff_time) -> int:
-    counter = 1
+def dfs(i, visited: set, number_vertex: int, graph, non_leaf):
     visited.add(i)
-    is_leaf = True
-    while counter < num_vertex + 1 and is_leaf:
-        if counter not in visited and graph.has_edge(i, counter):
-            is_leaf = False
-        counter += 1
-
-    if not is_leaf:
-        vc_size += 1
-        vertex_cov.append(i)
-
-    for k in range(num_vertex + 1):
-        if time.time() - start > cutoff_time:
-            break
-        if graph.has_edge(i, k) and k not in visited:
-            vc_size = dfs(k, visited, graph, vertex_cov, num_vertex, vc_size, start, cutoff_time)
-
-    return vc_size
+    for neighbor in list(graph[i]):
+        if neighbor not in visited:
+            visited.add(neighbor)
+            non_leaf.add(i)
+            dfs(neighbor, visited, number_vertex, graph, non_leaf)
